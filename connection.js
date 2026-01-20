a6bd21ed16b36b8a790ba4b409263144facca8b2
aresakshi <aresadhana04@gmail.com>
2026-01-20 19:39:20 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 const util = require("util");
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root", 
-    database: "salon_project_main2"
+    host: "b6efrhaytyiqxnij2p8l-mysql.services.clever-cloud.com",
+    user: "uhkomcjepmyui83f",
+    password: "j968dA3kQm20kylTL4WW", 
+    database: "b6efrhaytyiqxnij2p8l"
 });
 
 const exe = util.promisify(conn.query).bind(conn);
